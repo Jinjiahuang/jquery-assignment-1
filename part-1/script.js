c2941e77433c04245c7de05848e65cc34c42f84d
Jinjia Huang <jhuang64@id.iit.edu>
2017-04-28 01:52:53 -0500

    jquery assignment

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,20 @@
-// Do your work in this file.
+var view = $('<div class="view"></div>');
+$('body').append(view);
+view.append('<div class="grass"></div>');
+view.append('<div class="sun"></div>');
+view.append('<div class="mountain"></div>');
+$('.mountain').append('<div class="mountain-top"></div>');
+
+var i = 1;
+while (i < 6) {
+    $(".sun").append('<div class="ray-' + i + '"></div>');
+    i++;
+}
+
+var i = 1;
+do {
+    $(".mountain-top").append('<div class="mountain-cap-' + i + '"></div>');
+    i++;
+}
+while (i < 4);
+
--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,15 @@
-// Do your work in this file.
+for (i = 0; i < 196; i++) {
+    if (i % 2) {
+        $('body').append($('<div class="box type-2"></div>'))
+    }
+    else {
+        $('body').append($('<div class="box type-1"></div>'))
+    }
+};
+
+$('.box').click(function(){
+    if ($(this).hasClass('clicked')){
+        $(this).removeClass('clicked')
+    }
+    else {$(this).addClass('clicked')}
+});
